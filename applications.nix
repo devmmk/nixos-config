1dba1a5fcf0925581f3a56bbc350d18a6abf8c66
devmmk <mahdi.kashani2006@gmail.com>
2024-09-23 21:25:24 +0330

    First commit

---
+++ applications.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    vim
+    neovim
+    wget
+    zsh
+    git
+    gh
+    neofetch
+    htop
+    btop
+    vscode
+    chromium
+    telegram-desktop
+    nekoray
+    spotify
+    cinnamon.warpinator
+  ];
+}
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:/nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+
+    homeConfigurations = {
+      penguin = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+
+  };
+}
+
