006aa286db0050db6549706563d65a17684c2e68
devmmk <mahdi.kashani2006@gmail.com>
2024-09-25 04:17:00 +0330

    Initial Commit

---
+++ system/applications.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    vim
+    neovim
+    wget
+    zsh
+    git
+    gh
+    neofetch
+    htop
+    btop
+    vscode
+    chromium
+    telegram-desktop
+    nekoray
+    spotify
+    cinnamon.warpinator
+    vlc
+  ];
+}
